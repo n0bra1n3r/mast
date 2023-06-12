2cc2d0e0b68eacf741381fb45f4938d284f28b60
Ryan Blonna <n0.b741n37@gmail.com>
2023-06-13 09:01:27 +1000

    Add map operator and cleanup macros

--- src/mast/macros.nim
+++ src/mast/macros.nim
@@ -5,43 +5,24 @@ export macros except
   pragma,
   `pragma=`
 
-proc name*(node: NimNode): NimNode =
-  expectKind node: RoutineNodes + {nnkIdentDefs, nnkTypeDef}
-  case node.kind
-  of nnkIdentDefs, nnkTypeDef:
-    result = node[0]
-    if result.kind == nnkPragmaExpr:
-      result = result[0]
-    if result.kind == nnkPostfix:
-      if result[1].kind == nnkAccQuoted:
-        result = result[1][0]
-      else:
-        result = result[1]
-    elif result.kind == nnkAccQuoted:
-      result = result[0]
-  else:
-    result = macros.name(node)
+proc `...`*(node: NimNode): seq[NimNode] =
+  for child in node:
+    result.add child
 
-proc pragma*(node: NimNode): NimNode =
-  expectKind node: RoutineNodes + {nnkIdentDefs, nnkTypeDef, nnkProcTy}
-  case node.kind
-  of nnkIdentDefs, nnkTypeDef:
-    if node[0].kind == nnkPragmaExpr:
-      result = node[0][1]
-  else:
-    result = macros.pragma(node)
+template `~>.`*(node: NimNode, field: untyped{nkIdent}): seq[NimNode] =
+  var result = newSeq[NimNode]()
+  for child in node:
+    result.add child.field
+  result
 
-proc `pragma=`*(node, pragma: NimNode) =
-  expectKind node: RoutineNodes + {nnkIdentDefs, nnkTypeDef, nnkProcTy}
-  expectKind pragma: nnkPragma
-  case node.kind
-  of nnkIdentDefs, nnkTypeDef:
-    if node[0].kind == nnkPragmaExpr:
-      node[0][1] = pragma
-    else:
-      node[0] = nnkPragmaExpr.newTree(node[0], pragma)
-  else:
-    macros.`pragma=`(node, pragma)
+template `~>`*(
+  node: NimNode,
+  call: untyped{nkCall|nkCallStrLit|nkCommand}
+): seq[NimNode] =
+  var result = newSeq[NimNode]()
+  for it {.inject.} in node:
+    result.add call
+  result
 
 proc copyIdentDefs*(node: NimNode): NimNode =
   expectKind node: {
@@ -64,6 +45,30 @@ proc copyIdentDefs*(node: NimNode): NimNode =
     else:
       result.add identDef
 
+proc name*(node: NimNode): NimNode =
+  expectKind node: RoutineNodes + {
+    nnkExprColonExpr,
+    nnkIdent,
+    nnkIdentDefs,
+    nnkTypeDef,
+  }
+  case node.kind
+  of nnkExprColonExpr, nnkIdentDefs, nnkTypeDef:
+    result = node[0]
+    if result.kind == nnkPragmaExpr:
+      result = result[0]
+    if result.kind == nnkPostfix:
+      if result[1].kind == nnkAccQuoted:
+        result = result[1][0]
+      else:
+        result = result[1]
+    elif result.kind == nnkAccQuoted:
+      result = result[0]
+  of nnkIdent:
+    result = node
+  else:
+    result = macros.name(node)
+
 proc ofInherit*(node: NimNode): NimNode =
   expectKind node: nnkTypeDef
   expectKind node[2]: nnkObjectTy
@@ -74,3 +79,72 @@ proc `ofInherit=`*(node: NimNode, ofInheritNode: NimNode) =
   expectKind node[2]: nnkObjectTy
   expectKind ofInheritNode: nnkOfInherit
   node[2][1] = ofInheritNode
+
+proc pragma*(node: NimNode): NimNode =
+  expectKind node: RoutineNodes + {nnkIdentDefs, nnkTypeDef, nnkProcTy}
+  case node.kind
+  of nnkIdentDefs, nnkTypeDef:
+    if node[0].kind == nnkPragmaExpr:
+      result = node[0][1]
+  else:
+    result = macros.pragma(node)
+
+proc `pragma=`*(node, pragma: NimNode) =
+  expectKind node: RoutineNodes + {nnkIdentDefs, nnkTypeDef, nnkProcTy}
+  expectKind pragma: nnkPragma
+  case node.kind
+  of nnkIdentDefs, nnkTypeDef:
+    if node[0].kind == nnkPragmaExpr:
+      node[0][1] = pragma
+    else:
+      node[0] = nnkPragmaExpr.newTree(node[0], pragma)
+  else:
+    macros.`pragma=`(node, pragma)
+
+proc regenSyms*(node: NimNode): NimNode =
+  let procNodes = {
+    nnkConverterDef,
+    nnkFuncDef,
+    nnkIteratorDef,
+    nnkMethodDef,
+    nnkProcDef,
+  }
+  let varNodes = {
+    nnkConstSection,
+    nnkFormalParams,
+    nnkLetSection,
+    nnkVarSection,
+  }
+  expectKind node: procNodes + varNodes
+
+  proc regenIdentDefs(kind: NimSymKind, identDefs: NimNode): NimNode =
+    result = copyIdentDefs identDefs
+    for i, def in enumerate result:
+      if def.kind == nnkIdentDefs:
+        result[i][0] = genSym(kind, def[0].repr)
+      else:
+        result[i] = def
+
+  let kind = case node.kind
+    of nnkConverterDef: nskConverter
+    of nnkConstSection: nskConst
+    of nnkFormalParams: nskParam
+    of nnkFuncDef: nskFunc
+    of nnkIteratorDef: nskIterator
+    of nnkLetSection: nskLet
+    of nnkMethodDef: nskMethod
+    of nnkProcDef: nskProc
+    of nnkVarSection: nskVar
+    else: nskUnknown
+
+  if node.kind in varNodes:
+    result = regenIdentDefs(kind, node)
+  elif node.kind in procNodes:
+    result = copy node
+    result.name = genSym(kind, result.name.repr)
+    result.params = regenSyms(result.params)
+
+proc shift*(node: NimNode): NimNode =
+  expectMinLen node: 1
+  result = node[0]
+  node.del 0
